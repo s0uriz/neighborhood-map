b98eaa8e6d179f327e020ccd5e9d7b80d3c62fd7
s0uriz <ua87anna@gmail.com>
2018-07-31 17:09:16 +0300

    - add google-maps-react to project - create Map.js component

---
+++ src/components/Map.js
@@ -0,0 +1,61 @@
+import React, { Component } from "react";
+import { Map, InfoWindow, Marker, GoogleApiWrapper } from "google-maps-react";
+
+//note: code formatted for ES6 here
+export class MapContainer extends Component {
+  state = {
+    showingInfoWindow: false,
+    activeMarker: {},
+    selectedPlace: {}
+  };
+
+  onMarkerClick = (props, marker, e) =>
+    this.setState({
+      selectedPlace: props,
+      activeMarker: marker,
+      showingInfoWindow: true
+    });
+
+  onMapClicked = props => {
+    if (this.state.showingInfoWindow) {
+      this.setState({
+        showingInfoWindow: false,
+        activeMarker: null
+      });
+    }
+  };
+
+  render() {
+    const style = {
+      width: "100%",
+      height: "100%"
+    };
+    return (
+      <Map
+        google={this.props.google}
+        onClick={this.onMapClicked}
+        style={style}
+        initialCenter={{
+          lat: 40.854885,
+          lng: -88.081807
+        }}
+        zoom={15}
+      >
+        <Marker onClick={this.onMarkerClick} name={"Current location"} />
+
+        <InfoWindow
+          marker={this.state.activeMarker}
+          visible={this.state.showingInfoWindow}
+        >
+          <div>
+            <h1>{this.state.selectedPlace.name}</h1>
+          </div>
+        </InfoWindow>
+      </Map>
+    );
+  }
+}
+
+export default GoogleApiWrapper({
+  apiKey: "AIzaSyBBJzSPHKD0QIhvL89QsoK_BX7SqPZTYTI"
+})(MapContainer);
